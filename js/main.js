34a588eec10e6936a945c6515792228fef992214
zzinis <lizzyin@naver.com>
2021-10-28 21:28:42 +0900

    main menu department 추가

--- js/main.js
+++ js/main.js
@@ -10,4 +10,20 @@ btnCall.onclick = function(e){
     menuMo.classList.toggle("on"); 
 }
 
+$("#gnb>li").on("mouseenter", function(){
+    $(this).find(".sub").show();
+});
 
+$("#gnb>li").on("mouseleave", function(){
+    $(this).find(".sub").hide();
+})
+
+$("#gnb>li").each(function(index){
+    $("#gnb>li").eq(index).find("a").on("focusin", function(){
+        $("#gnb>li").eq(index).find(".sub").show();         
+    });
+
+    $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
+        $("#gnb>li").eq(index).find(".sub").hide();
+    });
+});
