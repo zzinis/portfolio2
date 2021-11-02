94716e91aa3d32493b678fd5a7795af4fef692a2
zzinis <lizzyin@naver.com>
2021-11-02 14:32:23 +0900

    gallery pop 수정

--- js/visual.js
+++ js/visual.js
@@ -1,33 +1,37 @@
 
-// const h1 = $(".txt h1");
-
-// $(h1).each(function(index, data){
-//     letter($(data), 0.1);
-// });
+const h1 = $(".txt h1");
 
 
+window.onload = (function(){
+    $(h1).each(function(index, data){
+        letter($(data), 0.1);
+    });
+    
+})
 
-// function letter(el, interval){
-//     if(el ===undefined) {
-//         console.error("첫번째 인수로 선택자를 지정해주세요!!");
-//         return;
-//     }
-//     if(interval === undefined) interval = 0;
 
-//     const txt = el.text();
-//     el.text("");
-//     let num=0;
 
-//     for(let letter of txt){
-//         el.append(
-//             $("<span>")
-//                 .text(letter)
-//                 .css({
-//                     "transition-delay": `${num*interval}s`,
-//                     "display": "inline-block" 
-//                 })
-//         );
-//         num++;
-//     }
-// }
+function letter(el, interval){
+    if(el ===undefined) {
+        console.error("첫번째 인수로 선택자를 지정해주세요!!");
+        return;
+    }
+    if(interval === undefined) interval = 0;
+
+    const txt = el.text();
+    el.text("");
+    let num=0;
+
+    for(let letter of txt){
+        el.append(
+            $("<span>")
+                .text(letter)
+                .css({
+                    "transition-delay": `${num*interval}s`,
+                    "display": "inline-block" 
+                })
+        );
+        num++;
+    }
+}
 
